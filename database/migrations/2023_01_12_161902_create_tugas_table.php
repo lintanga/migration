28b55063a83df8a396e7e668ddef2f94d3639f4e
lintanga <121338736+lintanga@users.noreply.github.com>
2023-01-12 18:01:51 +0700

    init

---
+++ database/migrations/2023_01_12_161902_create_tugas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tugas__db', function (Blueprint $table) {
+            $table->increment('id');
+            $table->string('nama')->unique();
+            $table->string('jenis kelamin');
+            $table->sring('alamat');
+            $table->integer('semester');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tugas');
+    }
+};
